d2ae8918ddc9005799ca46c1d9d893c302ed2e96
jonam <joshi.manoj@gmail.com>
2013-12-12 18:49:30 -0800

    Added some example document
    mongo document

--- mlogger/src/test/java/org/redbasin/mlogger/MLoggerTest.java
+++ mlogger/src/test/java/org/redbasin/mlogger/MLoggerTest.java
@@ -32,7 +32,39 @@ public class MLoggerTest
     }
 
     /**
-     * Rigourous Test :-)
+     * A document in mongo that looks like below should be created:
+     * 
+     * <pre>
+     > db.DEBUGfeature.stats()
+{
+	"ns" : "log.DEBUGfeature",
+	"count" : 3,
+	"size" : 1488,
+	"avgObjSize" : 496,
+	"storageSize" : 8192,
+	"numExtents" : 1,
+	"nindexes" : 10,
+	"lastExtentSize" : 8192,
+	"paddingFactor" : 1,
+	"systemFlags" : 1,
+	"userFlags" : 0,
+	"totalIndexSize" : 81760,
+	"indexSizes" : {
+		"_id_" : 8176,
+		"exception.lineNumber_1" : 8176,
+		"exception.className_1" : 8176,
+		"exception.fileName_1" : 8176,
+		"exception.methodName_1" : 8176,
+		"cause.lineNumber_1" : 8176,
+		"cause.className_1" : 8176,
+		"cause.fileName_1" : 8176,
+		"cause.methodName_1" : 8176,
+		"message_1" : 8176
+	},
+	"ok" : 1
+}
+
+     * </pre>
      */
     public void testApp() throws UnknownHostException
     {
